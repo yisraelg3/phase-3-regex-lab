6d6bd9d6268d777c290ed67d7c2649b5ac47019a
Yisrael Gurkow <yisrelg3@gmail.com>
2021-06-27 02:22:50 -0400

    commit tree 73bb6340206731a296f924ed06492e5cc73db806

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,21 @@
+require 'pry'
 def starts_with_a_vowel?(word)
-
+    match = word.match(/^[aeiouAEIOU]/)
+    !match.nil?
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+    text.scan(/un+\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+    text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+    !!text.match(/\A[A-Z]+.+\W\z/)
 end
 
 def valid_phone_number?(phone)
-
+    !phone.gsub(/\D/,'').scan(/^\d{10}$/).empty?
 end
